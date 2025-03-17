3f57b418aa56702e808b5f67a11d730d06f1586c
sjndowair <wodnjsdl1125@naver.com>
2025-03-17 20:03:00 +0900

    문제 풀이

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const a = 1;
-const b = 2;
+const solution = (a, b) => {
+  const isSort = a.sort((a, b) => a - b);
+  const isSort2 = b.sort((a, b) => b-a);
+  // console.log(isSort, isSort2)
+    let num = 0;
 
-console.log(a + b);
+  for(let i = 0; i < isSort.length; i++){
+        num += isSort[i] * isSort2[i]
+    }
+return num
+};
+
+
+
+console.log(solution([2,5,7],[2,5,7]))
