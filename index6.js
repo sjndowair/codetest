d302afc5f07794630392f1cc4a02149e7a2585d5
sjndowair <wodnjsdl1125@naver.com>
2025-03-18 20:44:32 +0900

    같은 숫자는 싫어 문제 풀이

--- index6.js
+++ index6.js
@@ -1,3 +1,6 @@
+//정수로 이루어진 배열 numbers가 있습니다. 배열 의 각 원소들에 대해 자신보다 뒤에 있는 숫자 중에서 자신보다 크면서 가장 가까이 있는 수를 뒷 큰수라고 합니다.
+//정수 배열 numbers가 매개변수로 주어질 때, 모든 원소에 대한 뒷 큰수들을 차례로 담은 배열을 return 하도록 solution 함수를 완성해주세요. 단, 뒷 큰수가 존재하지 않는 원소는 -1을 담습니다.
+
 function solution(numbers) {
   const result = Array(numbers.length).fill(-1);
   console.log(result);
@@ -15,3 +18,11 @@ function solution(numbers) {
 }
 
 console.log(solution([2, 3, 3, 5]));
+
+//1. numbers 배열의 길이와 같은 -1을 담은 배열을 생성한다.
+//2. stack 배열을 생성한다.
+//3. numbers 배열을 순회하며 stack 배열의 마지막 요소와 비교한다.
+//4. 만약 stack 배열의 마지막 요소가 numbers 배열의 현재 요소보다 작다면, stack 배열의 마지막 요소를 제거하고, result 배열의 현재 요소를 numbers 배열의 현재 요소로 변경한다.
+//5. stack 배열에 현재 요소를 추가한다.
+//6. 만약 stack 배열이 비어있다면, result 배열의 현재 요소를 -1로 변경한다.
+//7. result 배열을 반환한다.
