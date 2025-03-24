4f8826d6e5a39ae00768b0bada3adc6630bd1f34
sjndowair <wodnjsdl1125@naver.com>
2025-03-24 22:15:09 +0900

    stack을 활용한 문제 풀이 완료

--- index13.js
+++ index13.js
@@ -15,3 +15,7 @@ const solution = (num) => {
 
 console.log(solution(78));
 console.log(solution(15));
+
+const dd = [1, 2, 3, 4, 5];
+console.log(dd.slice(0, 3));
+console.log(dd);
--- index14.js
+++ index14.js
@@ -2,3 +2,20 @@
 //예를 들어, 문자열 S = baabaa 라면
 //b aa baa → bb aa → aa →
 //의 순서로 문자열을 모두 제거할 수 있으므로 1을 반환합니다.
+const solution = (str) => {
+  const arr = [...str];
+  let count = 0;
+  const stack = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (stack.length > 0 && stack[stack.length - 1] === arr[i]) {
+      stack.pop();
+    } else {
+      stack.push(arr[i]);
+    }
+  }
+
+  return stack.length === 0 ? 1 : 0;
+};
+
+console.log(solution("baabaa"));
