5478fc4bf0bd7ca1e5a04b7a5d2e5990df137818
sjndowair <wodnjsdl1125@naver.com>
2025-03-24 21:00:13 +0900

    다음 큰 숫자 찾기 문제 풀이

--- index13.js
+++ index13.js
@@ -1,20 +1,17 @@
-// const a = 10 >> 1;
-// console.log(a);
-
-// const b = 10 >> 2;
-// console.log(b);
-
-// const c = 10 >> 3;
-// console.log(c);
-
-const countOne = (num) => {
+const isNum = (num) => {
   return num.toString(2).split("1").length - 1;
 };
 
-console.log(countOne(10));
+const solution = (num) => {
+  let nowNum = isNum(num);
+  let nextNum = num + 1;
+  while (true) {
+    if (nowNum === isNum(nextNum)) {
+      return nextNum;
+    }
+    nextNum++;
+  }
+};
 
-const a = 1111111;
-console.log(a.toString(2).split("1"));
-
-const b = 121212;
-console.log(b.toString(2).split("1"));
+console.log(solution(78));
+console.log(solution(15));
