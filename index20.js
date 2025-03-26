7008e152d4652b32a29b44d64ee98251684dff61
sjndowair <wodnjsdl1125@naver.com>
2025-03-26 19:31:28 +0900

    문제 풀이 해결

--- index20.js
+++ index20.js
@@ -1,36 +1,39 @@
-const solution = (n) => {
-  let count = 0;
-  const answer = new Array(n).fill(0);
-  const arr = [];
-  for (let i = 0; i < answer.length; i++) {
-    if (answer[i] === answer[0]) {
-      count++;
-      arr.push(...answer.slice(i + 1));
-    } else {
-      answer = arr.push(answer.slice(arr.length * 2));
-      if (arr.length === answer.length && arr.length > answer.length) {
-        break;
-      }
-    }
-  }
-  let last = answer.length - arr.length;
-  console.log(answer, arr, last, count);
-  return (count += last);
-};
+// const solution = (n) => {
+//   let count = 0;
+//   const answer = new Array(n).fill(0);
+//   const arr = [];
+//   for (let i = 0; i < answer.length; i++) {
+//     if (answer[i] === answer[0]) {
+//       count++;
+//       arr.push(...answer.slice(i + 1));
+//     } else {
+//       answer = arr.push(answer.slice(arr.length * 2));
+//       if (arr.length === answer.length && arr.length > answer.length) {
+//         break;
+//       }
+//     }
+//   }
+//   let last = answer.length - arr.length;
+//   console.log(answer, arr, last, count);
+//   return (count += last);
+// };
 
-console.log(solution(2));
+// console.log(solution(2));
 
-function solution2(n) {
-  let count = 0;
-  while (n > 0) {
-    if (n % 2 === 1) {
-      count++;
-      n--;
-    } else {
-      n /= 2;
-    }
-  }
-  return count;
-}
+// function solution2(n) {
+//   let count = 0;
+//   while (n > 0) {
+//     if (n % 2 === 1) {
+//       count++;
+//       n--;
+//     } else {
+//       n /= 2;
+//     }
+//   }
+//   return count;
+// }
 
-console.log(solution2(4)); // 2
+// console.log(solution2(4)); // 2
+
+const a = [1, 3, 2, 5, 4, 5, 2, 3];
+console.log(a.filter((item, index) => a.indexOf(item) === index));
